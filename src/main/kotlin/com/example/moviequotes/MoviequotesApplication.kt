27f2dab85642ee08917dca884bf45394dc42abfd
aaayushm23 <130461281+aaayushm23@users.noreply.github.com>
2025-08-11 13:18:32 +0200

    git commit -m "Initial commit: Set up Spring Boot Kotlin project for Movie Quotes API"

---
+++ src/main/kotlin/com/example/moviequotes/MoviequotesApplication.kt
@@ -0,0 +1,11 @@
+package com.example.moviequotes
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MoviequotesApplication
+
+fun main(args: Array<String>) {
+	runApplication<MoviequotesApplication>(*args)
+}
